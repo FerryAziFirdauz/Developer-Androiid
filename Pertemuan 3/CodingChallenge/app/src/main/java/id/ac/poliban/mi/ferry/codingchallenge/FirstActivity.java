4d2fb0e3dfa0b93d5b7c56dc5d334a995320780b
FerryAziFirdauz <ferryazifirdauz@gmail.com>
2023-01-16 12:44:17 +0800

    first commit

---
+++ Pertemuan 3/CodingChallenge/app/src/main/java/id/ac/poliban/mi/ferry/codingchallenge/FirstActivity.java
@@ -0,0 +1,16 @@
+package id.ac.poliban.mi.ferry.codingchallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
